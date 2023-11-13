cd9bb7341998ea75807d5d3aabd1d08b89b232c4
despe-reymondchristian <chan.despe@gmail.com>
2023-11-13 15:27:21 +0800

    update file

--- despe_reymondchristian/script.js
+++ despe_reymondchristian/script.js
@@ -14,10 +14,56 @@ function onTextChange() {
 function addComment() {
     let onInputName = document.querySelector('#name').value;
     let onInputMessage = document.querySelector('#message').value;
-    let commentSection = document.querySelector('.comments');
-    const comment = document.createElement('p');
+    let commentSection = document.querySelector('.comments-section');
+    const sortOrder = document.querySelector('#sort_order').value;
 
-    comment.textContent = `${onInputName}: ${onInputMessage}`;
+    const existingComments = JSON.parse(localStorage.getItem('comments')) || [];
 
-    commentSection.appendChild(comment);
-}   
+    const newComment = {
+        name: onInputName,
+        message: onInputMessage,
+        timestamp: new Date().getTime() // Adding a timestamp
+    };
+
+    existingComments.push(newComment);
+
+    localStorage.setItem('comments', JSON.stringify(existingComments));
+
+    displayComments(commentSection, sortOrder);
+
+    document.querySelector('#name').value = '';
+    document.querySelector('#message').value = '';
+}
+
+function displayComments(commentSection, sortOrder = 'asc') {
+
+    commentSection.innerHTML = '';
+
+    const existingComments = JSON.parse(localStorage.getItem('comments')) || [];
+
+    document.querySelector('#sort_order').value = sortOrder;
+
+    const sortedComments = existingComments.sort((a, b) => {
+        return sortOrder === 'asc' ? a.timestamp - b.timestamp : b.timestamp - a.timestamp;
+    });    
+
+    sortedComments.forEach(comment => {
+        const commentBox = document.createElement('span');
+        const commentElement = document.createElement('p');
+        commentElement.textContent = `${comment.name}: ${comment.message}`;
+        commentBox.appendChild(commentElement);
+        commentSection.appendChild(commentBox);
+    });
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const commentSection = document.querySelector('.comments-section');
+    displayComments(commentSection);
+
+    document.querySelector('#sort_order').addEventListener('change', function() {
+        const sortOrder = this.value;
+        displayComments(commentSection, sortOrder);
+    });
+});
+
