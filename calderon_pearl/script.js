7e6746df398fff3cb829faac4a04c672c91df898
Pearl <calderon.pearl12@gmail.com>
2023-11-13 12:18:15 +0800

    push folder

---
+++ calderon_pearl/script.js
@@ -0,0 +1,32 @@
+/* function onTextChange not working */
+
+function onTextChange() {
+    let name = document.querySelector("#name").value;
+    let comments = document.querySelector("#comments").value;
+    let commentButton = document.querySelector("#comment_button");
+
+    if (comments.length > 0 && name.length > 0) {
+        commentButton.disabled = false;
+    }
+    else {
+        commentButton.disabled = true;
+    }
+}
+
+function addComment(event) {
+    event.preventDefault();
+    let onAddName = document.querySelector('#name').value;
+    let onAddMessage = document.querySelector('#comments').value;
+    let commentsSection = document.querySelector('.comments-section');
+    let list = document.querySelector('#list');
+    let li = document.createElement('li');
+    li.textContent = `${onAddName}: ${onAddMessage}`;
+    list.appendChild(li);
+    commentsSection.append(list);
+
+    document.querySelector('#name').value = '';
+    document.querySelector('#comments').value = '';
+    document.querySelector('#comment_button').disabled = true;
+}
+
+document.querySelector('#comment_form').addEventListener('submit', addComment);
